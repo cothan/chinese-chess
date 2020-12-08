75d382e470c9f8d9ba0c189f0170ba62011b58c3
cothan <cothannguyen@gmail.com>
2020-12-08 18:54:41 -0500

    tested store

--- app/http/controllers/GameController.py
+++ app/http/controllers/GameController.py
@@ -10,6 +10,9 @@ from app.User import User
 from secrets import token_hex
 from time import time 
 
+def current_time():
+    return int(time())
+
 class GameController(Controller):
     """GameController Controller Class."""
 
@@ -22,44 +25,47 @@ class GameController(Controller):
         self.request = request
 
 
-    def show(self, view: View):
+    def show(self, view: View, request: Request):
         """
         Show all users so player can pick one user to play with
         """
         users = User.all()
         
-        return view.render("game", {'users': users})
+        return view.render("game", {'users': users, 'name': request.user().name})
     
     def store(self, request: Request, view: View):
         """
         Generate a new token, return to chess/@token/ @show
         """
-        friend = request.input('friend', clean=True, quotes=True)
-        # TODO: filter friend input 
-        if not User.find(friend):
-            return 'Invalid username'
-        friend_id = User.find(friend).id 
+        friend = request.input('friend', clean=True)
+        friend_id = User.where('name', friend).limit(1).first()
+
+        if not friend_id:
+            return 'Username is not exists'
+        
+        friend_id = friend_id.id 
+       
         token = token_hex(16)
+       
         Table.create(
             user_id = request.user().id,
             oppo_id = friend_id,
             token = token, 
             completed = False,
-            last_move_timestamp = int(time),
+            last_move_timestamp = current_time(),
             next_id = request.user().id,
         )       
         return request.redirect("/play/@token", {'token': token})
 
     def resume(self, request: Request, view: View):
-        token = request.input('token')
-        #TODO: filter token 
+        token = request.input('token', clean=True)
         
-        table = Table.find(token)
+        table = Table.where('token', token).limit(1).first()
         if not table:
             return "token is not exist"
         
         # Check timestamp
-        timestamp = int(time) - table.last_move_timestamp
+        timestamp = current_time() - table.last_move_timestamp
         
         if timestamp > 300: 
             table.completed = True
